cee50e93df96cc5cd609d75e502578c15fe0b460
Neetify7 <220405487+Neetify7@users.noreply.github.com>
2025-07-19 12:25:38 -0400

    Revert "Fix useAuth hook to use client-side navigation"
    This reverts commit 447145295f7c0b015ded370f57990cd3a9bad3d9.

--- src/utils/use-auth.ts
+++ src/utils/use-auth.ts
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import { SessionData } from "./auth";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export function useAuth() {
   const [session, setSession] = useState<SessionData | null>(null);
   const isAuthenticated = session && session.isAuthenticated;
   const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
 
   useEffect(() => {
     async function fetchSession() {
@@ -27,9 +26,9 @@ export function useAuth() {
     if (!isLoading && !isAuthenticated) {
       const redirectPath = window.location.pathname;
 
-      router.push(`/sign-in?redirect=${encodeURIComponent(redirectPath)}`);
+      redirect(`/sign-in?redirect=${encodeURIComponent(redirectPath)}`);
     }
-  }, [isLoading, isAuthenticated, router]);
+  }, [isLoading, isAuthenticated]);
 
   return { isAuthenticated, isLoading };
 }
